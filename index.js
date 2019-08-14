14c04f01c1c2f7f58dceec85c5122f47a612bca4
MANISHA KUMARI <manishakumari@MANISHAs-MacBook-Pro.local>
2019-08-14 10:47:27 +0530

    Setting up the views

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const port = 8002;
 const app = express();
 
 app.use('/', require('./routers/index'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err){
     if(err)
